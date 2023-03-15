6225f844a6ad8fb89acf243eaea4b7abcfc25a82
indrabhushan11 <55879201+indrabhushan11@users.noreply.github.com>
2023-03-15 19:01:15 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        imageView = findViewById(R.id.imageView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+                changeImage();
+            }
+        });
+    }
+    public void changeImage(){
+        imageView.setBackgroundResource(R.drawable.index2);
+    }
+}
